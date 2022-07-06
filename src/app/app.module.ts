b1ac3026f30d1943b4f203340ad526e07c8b98c7
abhxi-r-s <abhishekunni8281@gmail.com>
2022-07-06 21:54:54 +0530

    Login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginPgComponent } from './login-pg/login-pg.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPgComponent
   ],
   imports: [
     BrowserModule,
