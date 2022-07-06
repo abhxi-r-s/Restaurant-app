8c625fd633420fe535955dfa9d424d8b007b50ed
abhxi-r-s <abhishekunni8281@gmail.com>
2022-07-06 23:29:02 +0530

    Search component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginPgComponent } from './login-pg/login-pg.component';
 import { ViewfoodComponent } from './viewfood/viewfood.component';
+import { SearchRestaurantComponent } from './search-restaurant/search-restaurant.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginPgComponent,
-    ViewfoodComponent
+    ViewfoodComponent,
+    SearchRestaurantComponent
   ],
   imports: [
     BrowserModule,
